5bb4795956e799058190f1186a8eb48a37ff350a
Luciano <luciano.canuto@outlook.com.br>
2025-11-03 16:09:26 -0300

    Adicionado Filtro de meses

--- app/src/main/java/br/com/educacaocrescer/appcrescer/escola/ListarRegistrosActivity.kt
+++ app/src/main/java/br/com/educacaocrescer/appcrescer/escola/ListarRegistrosActivity.kt
@@ -1,6 +1,10 @@
 package br.com.educacaocrescer.appcrescer.escola
 
+import android.R
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -30,8 +34,45 @@ class ListarRegistrosActivity : AppCompatActivity() {
         binding.recyclerRegistros.layoutManager = LinearLayoutManager(this)
         binding.recyclerRegistros.adapter = adapter
 
-        carregarRegistros()
+        carregarRegistros() // carrega todos do Firestore
+        configurarSpinnerMeses() // configura o filtro
     }
+
+    private fun configurarSpinnerMeses() {val meses = listOf(
+        "Todos",
+        "Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho",
+        "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"
+    )
+
+        val adapterSpinner = ArrayAdapter(this, R.layout.simple_spinner_item, meses)
+        adapterSpinner.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        binding.spinnerMeses.adapter = adapterSpinner
+
+        binding.spinnerMeses.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?, view: View?, position: Int, id: Long
+            ) {
+                val mesSelecionado = position // 0 = Todos, 1 = Janeiro, 2 = Fevereiro...
+                filtrarRegistrosPorMes(mesSelecionado)
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {}
+        }
+    }
+
+    private fun filtrarRegistrosPorMes(mes: Int) {
+        val registrosFiltrados = if (mes == 0) {
+            listaRegistros // todos
+        } else {
+            listaRegistros.filter { registro ->
+                val mesRegistro = registro.data.substring(5, 7).toInt() // pega MM de yyyy-MM-dd
+                mesRegistro == mes
+            }
+        }
+        adapter = RegistroAdapter(registrosFiltrados)
+        binding.recyclerRegistros.adapter = adapter
+    }
+
 
     private fun carregarRegistros() {
         db.collection("criancas")
